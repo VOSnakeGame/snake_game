19f521ae634ed2895377067a403ca9ea3a1adb10
jtomic <revan811@gmail.com>
2017-10-27 00:40:39 +0200

    Added demo scene

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+
+    public float speed;
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        // Get reference to Transform Component of Game Object
+        myTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Horizontal axis is left and right arrows
+        // Get axis raw means the result can be one of three things: -1, 0 and 1
+        // -1 is for left, 1 for right, and 0 if nothing is pressed
+        int horizontal = (int) Input.GetAxisRaw("Horizontal");
+
+		if (horizontal != 0) {
+            // Move object to left or right
+            // Vector3.rigth is [1, 0, 0]
+            // We must multiply by Time.delta time to account for time between two frames
+            myTransform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
+        }
+	}
+}
