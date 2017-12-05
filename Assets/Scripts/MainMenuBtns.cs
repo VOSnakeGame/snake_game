6d7a44a5c4e7089951d6894cf98c628e1aab52c8
ifarszky <ifarszky@croz.net>
2017-12-06 00:48:25 +0100

    made main menu, buttons for quit, opening lvls, loading lvl 1 and playing lvl

---
+++ Assets/Scripts/MainMenuBtns.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBtns : MonoBehaviour {
+
+    public Button playBtn;
+    public Button quitBtn;
+
+    // Use this for initialization
+    void Start () {
+        Button playBtnClick = playBtn.GetComponent<Button>();
+        Button quitBtnClick = playBtn.GetComponent<Button>();
+        playBtnClick.onClick.AddListener(OpenLvlsListener);
+        quitBtnClick.onClick.AddListener(QuitGameListener);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OpenLvlsListener()
+    {
+        SceneManager.LoadScene("Lvls", LoadSceneMode.Single);
+    }
+
+    void QuitGameListener()
+    {
+        Application.Quit();
+    }
+
+}
