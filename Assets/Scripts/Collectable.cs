b571dc27301ce86fd24f7ba47cdef80f9bf2f4b3
jtomic <revan811@gmail.com>
2017-12-11 20:16:18 +0100

    Added bacon

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    public float rotationSpeed = 5f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+	}
+}
