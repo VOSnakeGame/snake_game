cba343ce8cfcee8bacc65248819df0f98ffdf55c
jtomic <revan811@gmail.com>
2017-12-18 23:40:21 +0100

    Fixed game manager

--- Assets/Scripts/ManageGame.cs
+++ Assets/Scripts/ManageGame.cs
@@ -7,26 +7,21 @@ using UnityEngine.SceneManagement;
 public class ManageGame : MonoBehaviour {
 
 	public GameObject gameOverScreen;
-	public Button restartBtn;
-	public Button quitBtn;	
-	
 
 	// Use this for initialization
 	void Start () {	
 		gameOverScreen.SetActive(false);	
-        restartBtn.onClick.AddListener(RestartGame);
-        quitBtn.onClick.AddListener(QuitGame);		
 	}
 
 	public void OnGameOver() {
 		gameOverScreen.SetActive(true);		
 	}
 
-	void RestartGame() {
+	public void RestartGame() {
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 
-	void QuitGame() {
+	public void QuitGame() {
 		SceneManager.LoadScene("Lvls", LoadSceneMode.Single);
 	}
 
